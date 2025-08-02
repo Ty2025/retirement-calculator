c1cc51be19c057f317527c56389abcb51117676a
Tyler Augustine <TAugustine@oldmutual.com>
2025-08-02 12:22:19 +0200

    build ui base form  layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
 
 function App() {
+
+  
  return (
   <>
-  <div>Box</div>
+  <form style={{ width: '50%', margin: 'auto', paddingTop: '20px' }}>
+  <div className="mb-3">
+    <label className="form-label">Current Age</label>
+    <input type="email" className="form-control" id="currentAgeInput"/>
+  </div>
+  <div className="mb-3">
+    <label className="form-label">Retirement Age</label>
+    <input type="password" className="form-control" id="retirementAgeInput"/>
+  </div>
+  <div className="mb-3">
+    <label className="form-label">Monthly Contributions</label>
+    <input type="password" className="form-control" id="monthlyContributionInput"/>
+  </div>
+  <div className="mb-3">
+    <label className="form-label">Expected Annual investment return %</label>
+    <input type="password" className="form-control" id="percentageInput"/>
+  </div>
+  
+  <button type="submit" className="btn btn-primary">Calculate</button>
+</form>
   </>
  )
 }
